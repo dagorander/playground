9f08916553fa2083e8a75c949123810622333945
Daniel Agorander <daniel.agorander@gmail.com>
2021-10-10 22:33:39 +0200

    Completed the d_control_flow exercise :

--- rust/crash_course/exercise/d_control_flow_strings/src/main.rs
+++ rust/crash_course/exercise/d_control_flow_strings/src/main.rs
@@ -37,7 +37,10 @@ fn sum() {
     // 2. Use a "for loop" to iterate through integers from 7 to 23 *inclusive* using a range
     // and add them all together (increment the `sum` variable).  Hint: You should get 255
     // Run it with `cargo run sum`
-    
+
+    for i in 7..24 {
+        sum += i;
+    }
 
     println!("The sum is {}", sum);
 }
@@ -49,6 +52,10 @@ fn double() {
     // by 2) until `x` is larger than 500.  Increment `count` each time through the loop. Run it
     // with `cargo run double`  Hint: The answer is 9 times.
 
+    while x < 500 {
+        x = x * 2;
+        count += 1;
+    }
 
     println!("You can double x {} times until x is larger than 500", count);
 }
@@ -57,8 +64,15 @@ fn count(arg: String) {
     // Challenge: Use an unconditional loop (`loop`) to print `arg` 8 times, and then break.
     // You will need to count your loops, somehow.  Run it with `cargo run bananas`
     //
-    // print!("{} ", arg); // Execute this line 8 times, and then break. `print!` doesn't add a newline.
+    
+    let mut count = 0;
 
-
-    println!(); // This will output just a newline at the end for cleanliness.
+    loop {
+        print!("{} ", arg); // Execute this line 8 times, and then break. `print!` doesn't add a newline.
+        println!(); // This will output just a newline at the end for cleanliness.
+        count += 1;
+        if count >= 8 {
+            break;
+        }
+    }
 }
